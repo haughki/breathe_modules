05742ffbda9e383f88e03258b78fc0badffc3d41
parkeh1 <parkeh1@gene.com>
2021-04-09 11:00:27 -0700

    - Significant edits to the "one close" "two close" commands

--- _files.py
+++ _files.py
@@ -8,9 +8,10 @@ def openFile(command):
 
 class OpenFilesRule(MappingRule):
     mapping = {
-        "file voice working": Function(openFile, command=r'notepad "C:\Users\hawk\Desktop\voice_working.txt"'),
+        "file voice working": Function(openFile, command=r'notepad "C:\Users\parkeh1\Documents\voice_working.txt"'),
         "file random strings": Function(openFile, command=r'notepad "D:\temp\random_string.txt'),
-        "file daily tasks": Function(openFile, command=r'"C:\Program Files\Windows NT\Accessories\wordpad.exe" "D:\Google Drive\working\daily tasks.rtf"'),
+        "file daily tasks": Function(openFile, command=r'"C:\Program Files\Windows NT\Accessories\wordpad.exe" "C:\Users\parkeh1\Documents\daily tasks.rtf"'),
+        "file questions": Function(openFile, command=r'"C:\Program Files\Windows NT\Accessories\wordpad.exe" "C:\Users\parkeh1\Documents\running questions.rtf"'),
     }
     extras = [Dictation("text"),
               ]
--- commands/core/temp_extra.py
+++ commands/core/temp_extra.py
@@ -10,23 +10,24 @@ def add_close(_node):
         number = "2"
     close_start = "{{c" + number + "::"
 
-    Key("c-c, del").execute()
-    # Key("c-c").execute()
+    Key("c-c/25").execute()
+    Key("del").execute()
     clipboard = Clipboard()
-    clipboard.copy_from_system()
+    clipboard.copy_from_system() # populate this clipboard instance
     print(clipboard)
     close_word = clipboard.get_text()
 
     last = close_word[-1]
     second_to_last = close_word[-2]
 
+    exclude_me = [",", ".", ";", "!", "?", ")"]
     s = ""
     if last == " ":
-        if second_to_last == "," or second_to_last == ".":
+        if second_to_last in exclude_me:
             s = close_start + close_word[:-2] + "}}" + second_to_last + " "
         else:
             s = close_start + close_word[:-1] + "}}" + " "
-    elif last == "," or last == ".":
+    elif last in exclude_me:
         s = close_start + close_word[:-1] + "}}" + last
     else:
         s = close_start + close_word + "}}"
